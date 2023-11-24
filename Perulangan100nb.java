98b92c1ad32decdf2d5191290aa798a3dbf1f45d
Nursita-G1A018092 <151905453+Nursita-G1A018092@users.noreply.github.com>
2023-11-24 13:19:06 +0700

    Create Perulangan100nb.java

---
+++ Perulangan100nb.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author anand
+ */
+public class Perulangan100nb {
+    public static void main(String[] args) {
+        String nama = "Nursita";
+        for(int a = 1 ; a<=100; a++){
+            if(a<10){
+                System.out.println(a);
+            }
+            else{
+                System.out.println(nama);
+            }
+        }
+    }
+}
